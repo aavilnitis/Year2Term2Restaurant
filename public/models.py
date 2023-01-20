40539153719619d004d3c566b4bf6ad8778e793c
Jaskirat <zjac299@live.rhul.ac.uk>
2023-01-20 10:11:04 +0000

    Creating models.py:
    I have made a menuItem class which has an id, name,
    price, description and type attributes.
    This will create the menuItem table in the database.db file.
    This table uses id as the primary key.
    It uses enum type to help differentiate between food and drinks.
    
    I have made a Order class which has an id, order_total
    and status attributes.
    This will create the Order table in the database.db file.
    It uses the id attribute as primary key.
    It uses enum status to help differentiate between complete and
    incomplete orders.

--- public/models.py
+++ public/models.py
@@ -7,7 +7,7 @@ class MenuItem(db.model):
     name = db.Column(db.String(100), nullable = False)
     price = db.Column(db.Float, nullable = False)
     description = db.Column(db.String(300))
-    type = db.Column(db.Enum('food', 'drink', name='menuItem_type'), nullable  = False)
+    type = db.Column(db.Enum('food', 'drink', name='menuItem_type'), nullable = False)
 
     def __init__(self, name, price, description, type):
         self.name = name
@@ -18,7 +18,7 @@ class MenuItem(db.model):
 class Order(db.model):
     id = db.Column(db.Integer, primary_key = True)
     order_total = db.Column(db.Float, nullable = False)
-    status = db.Column(db.Enum('complete', 'incomplete', name='order_status'), nullable  = False)
+    status = db.Column(db.Enum('complete', 'incomplete', name='order_status'), nullable = False)
     
     def __init__(self, order_total, status):
         self.order_total = order_total
