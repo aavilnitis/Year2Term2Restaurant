5849a5ef9f014d0596d68382391b69e4ef2cbf4a
Robert Marshall <ZKAC211@live.rhul.ac.uk>
2023-01-20 15:14:25 +0000

    Created the file populatemenu.sql which contains various SQL INSERT statements that fill up the rows of the table with unique food/drink items. Modified models.py, specifically the id column in the MenuItem object, so that it autoincrements upon inserting an object.

--- public/models.py
+++ public/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class MenuItem(db.model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     name = db.Column(db.String(100), nullable = False)
     price = db.Column(db.Float, nullable = False)
     description = db.Column(db.String(300))
---
+++ public/populatemenu.sql
@@ -0,0 +1,8 @@
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Coca-Cola', 2.99, 'Classic soda made with real sugar.', 'drink');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Iced Tea', 2.49, 'Refreshing iced tea made with real tea leaves.', 'drink');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Lemonade', 2.99, 'Tart and sweet homemade lemonade.', 'drink');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Toilet Water', 50000.99, 'It's better than you think', 'drink');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Cheeseburger', 8.99, 'Juicy beef patty topped with melted cheese and served on a sesame seed bun.', 'food');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('BLT', 6.99, 'Crispy bacon, lettuce, and tomato served on white toast.', 'food');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('Chalk', 777.99, 'It's just chalk.', 'food');
+INSERT INTO MenuItem (name, price, description, type) VALUES ('World's Largest Pepperoni Pizza', 99999.99, 'Our magnum opus, an eighty inch pizza. You won't get it through your door!', 'food');
