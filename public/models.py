42cc696002f1d435cfc41f045ca5e6a560b744aa
Jaskirat <zjac299@live.rhul.ac.uk>
2023-01-21 19:41:29 +0000

    Adding test_models.py:
    i have created unittest cases to check the menuItem model works
    i have also added a test case to show the sql file works.
    
    i had to change the sql file as  MenuItem is not a table,
    instead it reads menu_item as the table.
    
    i also had to change models.py to use the database created
    in __init__.py file.

--- public/models.py
+++ public/models.py
@@ -1,9 +1,8 @@
+from . import db
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
 class MenuItem(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     price = db.Column(db.Float, nullable = False)
     description = db.Column(db.String(300))
