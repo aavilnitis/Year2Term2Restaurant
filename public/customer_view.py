51aa00aacbebb04acab21e8816106156edf3a974
MrAbdullahAamir <106662754+MrAbdullahAamir@users.noreply.github.com>
2023-01-24 13:39:17 +0000

    Added home.html: I have made basic home page which extends base.html. I have modifified the route in customer_view to use the home.html.

--- public/customer_view.py
+++ public/customer_view.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 customer_view = Blueprint('customer_view', __name__)
 
 
 @customer_view.route('/')
 def home():
-    return "<h1>Home route</h1>"
+    return render_template("home.html")
