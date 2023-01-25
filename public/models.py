efe00a5d245d1d388a325fd579d546f71dbf7c27
Sachdeva Jaskirat (2020) ZJAC299 <Jaskirat.Sachdeva.2020@live.rhul.ac.uk>
2023-01-25 10:45:38 +0000

    Refactoring Orders Model: I have added default values for order_total and status. I have also added the relationship menu_items_list column in orders. The use of the association table order_menu_item and db.relationship, will allow a many to many relationship which will mean the menu_items_list column can hold more than one MenuItem.

--- public/models.py
+++ public/models.py
@@ -15,13 +15,21 @@ class MenuItem(db.Model):
         self.description = description
         self.type = type
 
+#association table
+order_menu_item = db.Table("order_menu_item",
+    db.Column("order_id", db.Integer, db.ForeignKey("orders.id"), primary_key=True),
+    db.Column("menu_item_id", db.Integer, db.ForeignKey("menu_items.id"), primary_key=True)
+)
+
 class Order(db.Model):
     __tablename__ = "orders"
     id = db.Column(db.Integer, primary_key = True)
-    order_total = db.Column(db.Float, nullable = False)
-    status = db.Column(db.Enum('complete', 'incomplete', name='order_status'), nullable = False)
+    menu_items_list = db.relationship("MenuItem", secondary = order_menu_item, back_populates="orders")
+    order_total = db.Column(db.Float, nullable = False, default = 0)
+    status = db.Column(db.Enum('complete', 'incomplete', name='order_status'), nullable = False, defualt = 'incomplete')
     
-    def __init__(self, order_total, status):
+    def __init__(self, menu_items_list, order_total = 0, status = 'incomplete'):
+        self.menu_items_list = menu_items_list
         self.order_total = order_total
         self.status = status
    
