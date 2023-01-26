2850f168c1f7dcdaa416107e92bc5db990692895
Johnny Xie <sichen.j.xie@foxmail.com>
2023-01-26 01:39:58 +0000

    Create a Flask route for the view_menu page

--- public/customer_view.py
+++ public/customer_view.py
@@ -6,3 +6,11 @@ customer_view = Blueprint('customer_view', __name__)
 @customer_view.route('/')
 def home():
     return render_template("home.html")
+
+@customer_view.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@customer_view.route('/menu')
+def menu():
+    return render_template("menu.html")
