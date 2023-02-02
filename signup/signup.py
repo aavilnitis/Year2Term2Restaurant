17f985ec517af37db89f2a3cba6879ab37717f0f
zkac401 <zkac401@live.rhul.ac.uk>
2023-02-02 11:53:26 +0000

    SignUp template check length of username, pwd Adding checks to the signup page to check if the username is longer than 5 characters, same goes for the password. I also added message flashing to the base.html template to allow me to flash an error or succesful message.

--- signup/signup.py
+++ signup/signup.py
@@ -1,12 +1,23 @@
-from flask import Blueprint, render_template, request, redirect, session, jsonify, url_for
+from flask import Blueprint, render_template, request, redirect, session, jsonify, url_for, flash
 from packages.models import MenuItem, Order, db
 from sqlalchemy.sql import text
 
 # register customer_view as a Flask Blueprint
 signup = Blueprint("signup", __name__, static_folder="static", template_folder="templates")
 
-@signup.route('/')
-def home():
+@signup.route('/', methods = ['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if len(username) < 5:
+            flash('Username must be longer than 5 characters!', category='error')
+            return redirect(url_for('signup.sign_up'))
+        if len(password) < 5:
+            flash('Password must be longer than 5 characters!', category='error')
+            return redirect(url_for('signup.sign_up'))
+        flash('User created succesfully', category='success')
+        return render_template("signup.html")
     return render_template("signup.html")
 
 
