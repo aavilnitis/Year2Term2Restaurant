238eada21e57c8caf7ce2c0cb687c51666a7eb9b
HELAL Youssef (2021) WKIS035 <youssef.helal.2021@live.rhul.ac.uk>
2023-02-06 16:04:20 +0000

    I created a flask route to confirm order, when you press on confirm order button, It should update the “status” field of the order object.

--- waiter/waiter.py
+++ waiter/waiter.py
@@ -25,3 +25,11 @@ def cancel_order(order_id):
     db.commit()
     return redirect(url_for('orders'))
 
+# Flask route to confirm order 
+# Update the status of the order
+@waiter.route('/confirm_order/<int:order_id>', methods=['POST'])
+def confirm_order(order_id):
+    order = Order.query.get(order_id)
+    order.status = "Confirmed"
+    db.session.commit()
+    return redirect(url_for('orders'))
