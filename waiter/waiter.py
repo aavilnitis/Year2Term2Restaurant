de9ff7c1f66a94c7afbbe3bfbe020ad36c364b36
Mohamed <ZJAC347@live.rhul.ac.uk>
2023-02-07 11:40:06 +0000

    Added code to ensure database is populated whenever /waiter/menu is accessed. Before this, whenever server was restarted /waiter/menu database was not repopulated.
    A quick fix was to go to /menu to repopulate the database. Better solution was for
    /waiter/menu to also repopulate the database.

--- waiter/waiter.py
+++ waiter/waiter.py
@@ -5,6 +5,12 @@ from sqlalchemy.sql import text
 # register customer_view as a Flask Blueprint
 waiter = Blueprint("waiter", __name__, static_folder="static", template_folder="templates")
 
+def populate_menu():
+    with open("static/SQL_Inserts/populatemenu.sql", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            db.session.execute(text(line))
+            db.session.commit()
 
 @waiter.route('/')
 def home():
@@ -13,11 +19,13 @@ def home():
 
 @waiter.route('/menu')
 def menu():
+    if MenuItem.query.filter_by(name = "Cheeseburger").first() == None:
+        populate_menu()
     menu_items = MenuItem.query.all()
     return render_template('waiter-menu.html', items=menu_items)
 
 # Route to add item into menu
-@waiter.route('/add_item.html')
+@waiter.route('/add_item')
 def additem():
     name = request.form['Name']
     price = request.form['Price']
@@ -26,6 +34,18 @@ def additem():
     cal = request.form['Cal']    # calories
     type = request.form['Type']
     form_data = MenuItem(name, price, des, type)
-    db.session.add(form_data)   # add Item into db
-    db.session.commit()
     return render_template('add_item.html')
+
+
+
+
+
+ #   name = request.form['Name']
+  #  price = request.form['Price']
+   # des = request.form['Des']    # Description
+    #ingre = request.form['Ing']  # Ingredients
+   # cal = request.form['Cal']    # calories
+  #  type = request.form['Type']
+  #  form_data = MenuItem(name, price, des, type)
+  #  db.session.add(form_data)   # add Item into db
+#    db.session.commit()
