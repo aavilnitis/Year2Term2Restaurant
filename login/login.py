ed2ba8070e6b0f5699dbce246d55654662d6f5d7
Robert Marshall <ZKAC211@live.rhul.ac.uk>
2023-02-07 16:44:03 +0000

    Made improvements and bug fixes to the login.py file that handles the logic of authentication and routing. I have changed @app.route to @login so it works with the defined Blueprint. I have encoded the password with utf-8 like it is in signup.py. Added additional styling to login.html and changed the email field to username to better reflect the structure of the Users table.

--- login/login.py
+++ login/login.py
@@ -5,21 +5,22 @@ import bcrypt
 
 login = Blueprint("Login",__name__,static_folder="static",template_folder="templates")
 
-@app.route('/', methods=['GET', 'POST'])
+@login.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         # Get the entered username and password
         username = request.form['username']
-        password = request.form['password']
+        password = request.form['password'].encode('utf-8')
 
         # Fetch the user from the database based on the entered username
         user = User.query.filter_by(username=username).first()
 
         # If the user exists, check the entered password against the hashed password in the database
-        if user and bcrypt.check_password_hash(user.password, password):
-            return redirect(url_for('home'))
+        if user and bcrypt.check_password_hash(password, user.password.encode('utf-8')):
+            return redirect(url_for('home')) #return to home.html 
         else:
             # Incorrect credentials. Perhaps lock after some tries?
-            return render_template('login.html', error='Invalid username or password')
+            return render_template('login.html', error='Invalid username or password') 
 
     return render_template('login.html')
+    
