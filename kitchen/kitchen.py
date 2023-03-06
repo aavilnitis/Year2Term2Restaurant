a15072f80f3432df1c864731f391cc59a036be10
Robert Marshall <ZKAC211@live.rhul.ac.uk>
2023-03-06 15:58:32 +0000

    Added additional authentication to kitchen.py that ensures only kitchen staff can view the home page, notifications etc.

--- kitchen/kitchen.py
+++ kitchen/kitchen.py
@@ -6,7 +6,19 @@ from sqlalchemy.sql import text
 # register customer_view as a Flask Blueprint
 kitchen = Blueprint("kitchen", __name__, static_folder="static", template_folder="templates")
 
+def kitchenstaff_required(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if session.get('user') != 'kitchen_staff':
+            if session.get('user') == 'customer':
+                return redirect(url_for('customer.home'))
+            else: 
+                return "You do not have permission to view this page"
+        return func(*args, **kwargs)
+    return wrapper
+
 @kitchen.route('/')
+@kitchenstaff_required
 def home():
     return render_template("kitchen-home.html")
 
