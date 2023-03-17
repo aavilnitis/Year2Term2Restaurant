df1139597bad3781c73b65612d4ed0f714cb7320
HELAL Youssef (2021) WKIS035 <youssef.helal.2021@live.rhul.ac.uk>
2023-03-17 18:25:51 +0000

    added inline comments to routes to improve readability

--- login/login.py
+++ login/login.py
@@ -18,9 +18,11 @@ def login():
         # If the user exists, check the entered password against the hashed password in the database
         if found_user:
             if bcrypt.checkpw(password, found_user.password):
+                # If the entered password is correct, set session variables and redirect to appropriate page based on user type
                 session['user'] = found_user.user_type
                 session['user_id'] = found_user.id
                 print(found_user.id)
+                # Redirect the user to the appropriate page based on user type
                 if found_user.user_type == 'customer':
                     return redirect(url_for('customer.table_number'))
                 elif found_user.user_type == 'waiter':
@@ -30,15 +32,19 @@ def login():
                 elif found_user.user_type == 'admin':
                     return redirect(url_for('admin.home'))
                 else:
+                    # Something went wrong if we reach this point, so flash an error message and redirect to the login page
                     flash('Something went wrong!', category='error')
                     return redirect(url_for('login_view.login'))
             else:
+                # If the password is incorrect, flash an error message and redirect to the login page
                 flash('Incorrect password', category='error')
                 return redirect(url_for('login_view.login'))
         else:
+            # If the user is not found, flash an error message and redirect to the login page
             flash('User not found', category='error')
             return redirect(url_for('login_view.login'))
         
-        
+     # If the request method is GET, request render the login template    
     return render_template("login.html")
     
+   
