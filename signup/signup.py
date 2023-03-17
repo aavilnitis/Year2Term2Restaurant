8b6fe4e5c87683cea39a4149e7b4c5c377f38ca4
HELAL Youssef (2021) WKIS035 <youssef.helal.2021@live.rhul.ac.uk>
2023-03-17 18:08:47 +0000

    added inline comments to routes to improve readability

--- signup/signup.py
+++ signup/signup.py
@@ -9,12 +9,17 @@ signup = Blueprint("signup", __name__, static_folder="static", template_folder="
 @signup.route('/', methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
+        # get all existing customers' usernames from the database
         users = User.query.filter_by(user_type='customer').all()
         usernames = []
         for user in users:
             usernames.append(user.username)
+
+        # get new username and password from the form
         username = request.form.get('username')
         password = request.form.get('password')
+
+         # validate the username and password
         if len(username) < 5:
             flash('Username must be longer than 5 characters!', category='error')
             return redirect(url_for('signup.sign_up'))
@@ -24,14 +29,23 @@ def sign_up():
         if len(password) < 5:
             flash('Password must be longer than 5 characters!', category='error')
             return redirect(url_for('signup.sign_up'))
+        
+         # hash the password
         password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+         # create a new customer with the validated username and hashed password
         user = User(username, password, 'customer')
         db.session.add(user)
         db.session.commit()
+
+        # set the session variables for the new customer
         flash('User created succesfully', category='success')
         session['user'] = 'customer'
         session['user_id'] = user.id
+
+        # redirect to the next page, which is the table number selection page for the customer
         return redirect(url_for("customer.table_number"))
+     # render the sign-up page if the request method is GET
     return render_template("signup.html")
 
 
