ccff78d7a3c3a6b61fa13955b827fce949de2d64
zkac401 <d.abolinjs@gmail.com>
2023-03-20 16:31:37 +0000

    Refactoring - pydoc kitchen changeDelivery Added pydoc to our code to make it easier to read and keep up-to-date. This helps others understand what the code does and how to use it. It also makes updating the code easier and reduces mistakes, making our code better overall and removing code smells.

--- kitchen/kitchen.py
+++ kitchen/kitchen.py
@@ -62,7 +62,7 @@ def removeNotificationPage(notif_id):
         notif_id (id): The id of the Notification to remove
 
     Returns:
-        flask.Response: A redirect response to the kitchen staff home notifications page
+        flask.Response: A redirect response to the kitchen staff notifications page
     """
     notification = Notification.query.filter_by(id = notif_id).first()
     db.session.delete(notification)
@@ -86,34 +86,23 @@ def viewOrders():
     users = User.query.all()
     return render_template('kitchen-view-order.html', orders = orders, menu_items = menu_items, users = users)
 
-# Flask route to confirm order
-# Update the status of the order
-@kitchen.route('/confirm_order/<int:order_id>', methods=['POST'])
-@kitchenstaff_required
-def confirmOrder(order_id):
-    order = Order.query.get(order_id)
-    order.status = "confirmed"
-    db.session.commit()
-    return redirect(url_for('waiter.viewOrders'))
-
-# Flask route to cancel an order
-# Delete the order from the database
-@kitchen.route('/cancel_order/<int:order_id>', methods=['POST'])
-@kitchenstaff_required
-def cancelOrder(order_id):
-    order = Order.query.get(order_id)
-    if order:
-        for menu_item in order.order_menu_items:
-            db.session.delete(menu_item)
-        db.session.delete(order)
-        db.session.commit() 
-    return redirect(url_for('waiter.viewOrders'))
 
 # Flask route to mark order as delivered
 # Update the status of the order
 @kitchen.route('/change-delivery/<int:order_id>/<string:status>/<int:user_id>', methods=['POST'])
 @kitchenstaff_required
 def changeDelivery(order_id, status, user_id):
+    """Changes delivery status of an order by calling change_delivery and then 
+        redirects the kitchen staff member back to the orders page
+
+    Args:
+        order_id (id): The id of the Order which needs to be modified
+        status (enum): The status to which order delivery status needs to be changed to
+        user_id (_type_): The id of the customer who placed the order
+
+    Returns:
+        flask.Response: A redirect response to the kitchen staff orders page
+    """
     customer = User.query.get(user_id)
     table_num = customer.table_number
     change_delivery(order_id, status, table_num, user_id)
